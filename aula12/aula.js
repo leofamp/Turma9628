9c169eaf19721505c3c6685980b0b23e5137b437
leofamp <engleofamp@gmail.com>
2026-07-08 19:42:39 -0300

    aula 13

--- aula12/aula.js
+++ aula12/aula.js
@@ -4,7 +4,22 @@
 // let aprovado = true//boolean
 
 
-let n1 = 10**1000
+// let n1 = 10**1000
 
 
-console.log(n1)
+// console.log(n1)
+
+let n1 = prompt("Digite um número (ou -1 para sair):")
+n1 = parseInt(n1)
+
+let resultado = 0
+
+while (n1 !== -1) {
+    resultado += n1
+    
+    n1 = prompt("Digite um número (ou -1 para sair):")
+    n1 = parseInt(n1)
+}
+
+console.log("Resultado final:", resultado)
+
